74c75b637a0fa3684a9c5393024566e831d75e33
Brandon Williams <brandon@mystenlabs.com>
2025-01-21 08:36:11 -0600

    sui-sdk-types: add doc comments for the top-level and Address

--- crates/iota-sdk-types/src/lib.rs
+++ crates/iota-sdk-types/src/lib.rs
@@ -2,6 +2,67 @@
 // Modifications Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+//! Core type definitions for the IOTA blockchain.
+//!
+//! [IOTA] is a next-generation smart contract platform with high throughput,
+//! low latency, and an asset-oriented programming model powered by the Move
+//! programming language. This crate provides type definitions for working with
+//! the data that makes up the IOTA blockchain.
+//!
+//! [IOTA]: https://iota.org
+//!
+//! # BCS
+//!
+//! [BCS] is the serialization format used to represent the state of the
+//! blockchain and is used extensively throughout the IOTA ecosystem. In
+//! particular the BCS format is leveraged because it _"guarantees canonical
+//! serialization, meaning that for any given data type, there is a one-to-one
+//! correspondence between in-memory values and valid byte representations."_
+//! One benefit of this property is to allow different entities in the ecosystem
+//! to all agree on how a particular type should be interpreted and more
+//! importantly define a deterministic representation for hashing and signing.
+//!
+//! This library strives to guarantee that the types defined are fully
+//! BCS-compatible with the data that the network produces. The one caveat to
+//! this would be that as the IOTA protocol evolves, new type variants are added
+//! and older versions of this library may not support those newly
+//! added variants. The expectation is that the most recent release of this
+//! library will support new variants and types as they are released to IOTA's
+//! `testnet` network.
+//!
+//! See the documentation for the various types defined by this crate for an
+//! example of their BCS serialized representation. In addition to the format
+//! itself, some types have an extra layer of verification and may impose
+//! additional restrictions on valid byte representations above and beyond those
+//! already provided by BCS. In these instances the documentation for those
+//! types will clearly specify these additional restrictions.
+//!
+//! [BCS]: https://docs.rs/bcs
+//!
+//! # Feature flags
+//!
+//! This library uses a set of [feature flags] to reduce the number of
+//! dependencies and amount of compiled code. By default, no features are
+//! enabled which allows one to enable a subset specifically for their use case.
+//! Below is a list of the available feature flags.
+//!
+//! - `schemars`: Enables JSON schema generation using the [schemars] library.
+//! - `serde`: Enables support for serializing and deserializing types to/from
+//!   BCS utilizing [serde] library.
+//! - `rand`: Enables support for generating random instances of a number of
+//!   types via the [rand] library.
+//! - `hash`: Enables support for hashing, which is required for deriving
+//!   addresses and calculating digests for various types.
+//! - `proptest`: Enables support for the [proptest] library by providing
+//!   implementations of [proptest::arbitrary::Arbitrary] for many types.
+//!
+//! [feature flags]: https://doc.rust-lang.org/cargo/reference/manifest.html#the-features-section
+//! [serde]: https://docs.rs/serde
+//! [rand]: https://docs.rs/rand
+//! [proptest]: https://docs.rs/proptest
+//! [schemars]: https://docs.rs/schemars
+//! [proptest::arbitrary::Arbitrary]: https://docs.rs/proptest/latest/proptest/arbitrary/trait.Arbitrary.html
+
 #![cfg_attr(doc_cfg, feature(doc_cfg))]
 
 #[cfg(feature = "hash")]
