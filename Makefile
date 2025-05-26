MystenLabs/sui-rust-sdk

chore: rename sui-sdk to sui-sdk-types

Makefile
@@ -4,7 +4,7 @@ all:: ci
 
 .PHONY: check-features
 check-features:
-	$(MAKE) -C crates/sui-sdk check-features
+	$(MAKE) -C crates/sui-sdk-types check-features
 	$(MAKE) -C crates/sui-sdk-crypto check-features
 
 .PHONY: check-fmt
@@ -22,7 +22,7 @@ test:
 
 .PHONY: wasm
 wasm:
-	$(MAKE) -C crates/sui-sdk wasm
+	$(MAKE) -C crates/sui-sdk-types wasm
 	$(MAKE) -C crates/sui-sdk-crypto wasm
 
 .PHONY: doc

iotaledger/iota-rust-sdk

Makefile
@@ -4,7 +4,7 @@ all:: ci
 
 .PHONY: check-features
 check-features:
-	$(MAKE) -C crates/iota-sdk check-features
+	$(MAKE) -C crates/iota-sdk-types check-features
 	$(MAKE) -C crates/iota-sdk-crypto check-features
 
 .PHONY: check-fmt
@@ -22,7 +22,7 @@ test:
 
 .PHONY: wasm
 wasm:
-	$(MAKE) -C crates/iota-sdk wasm
+	$(MAKE) -C crates/iota-sdk-types wasm
 	$(MAKE) -C crates/iota-sdk-crypto wasm
 
 .PHONY: doc