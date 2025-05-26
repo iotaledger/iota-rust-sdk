MystenLabs/sui-rust-sdk

sui-sdk-crypto: introduce crate to handle signing and verifying

Makefile
@@ -5,6 +5,7 @@ all:: ci
 .PHONY: check-features
 check-features:
 	$(MAKE) -C crates/sui-sdk check-features
+	$(MAKE) -C crates/sui-sdk-crypto check-features
 
 .PHONY: check-fmt
 check-fmt:
@@ -22,6 +23,7 @@ test:
 .PHONY: wasm
 wasm:
 	$(MAKE) -C crates/sui-sdk wasm
+	$(MAKE) -C crates/sui-sdk-crypto wasm
 
 .PHONY: doc
 doc:

iotaledger/iota-rust-sdk

Makefile
@@ -4,7 +4,8 @@ all:: ci
 
 .PHONY: check-features
 check-features:
-	$(MAKE) -C crates/iota-rust-sdk check-features
+	$(MAKE) -C crates/iota-sdk check-features
+	$(MAKE) -C crates/iota-sdk-crypto check-features
 
 .PHONY: check-fmt
 check-fmt:
@@ -21,7 +22,8 @@ test:
 
 .PHONY: wasm
 wasm:
-	$(MAKE) -C crates/iota-rust-sdk wasm
+	$(MAKE) -C crates/iota-sdk wasm
+	$(MAKE) -C crates/iota-sdk-crypto wasm
 
 .PHONY: doc
 doc: