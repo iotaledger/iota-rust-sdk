94f74026d8e589c5cfe500c106b4ca70ab215583
Chloe Martin <chloedaughterofmars@gmail.com>
2025-06-03 16:51:09 +0200

    chore: Add missing license header

--- crates/iota-transaction-builder/src/unresolved.rs
+++ crates/iota-transaction-builder/src/unresolved.rs
@@ -1,3 +1,7 @@
+// Copyright (c) Mysten Labs, Inc.
+// Modifications Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 use iota_types::{Address, Command, ObjectDigest, ObjectId, TransactionExpiration, Version};
 
 // A potentially unresolved user transaction. Note that one can construct a
