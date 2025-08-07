ce48bd6239614f6b4dd7b5e94bd13760aa52fa72
Thibault Martinez <thibault@iota.org>
2025-08-07 15:27:19 +0200

    working test

--- bindings/go/README.md
+++ bindings/go/README.md
@@ -2,7 +2,7 @@
 
 First install [uniffi-bindgen-go](https://github.com/NordSecurity/uniffi-bindgen-go)
 
-Then compile the FFI crate to a dynamic library:
+Then compile the FFI crate to a dynamic library
 
 ```sh
 cargo build --all-features -p iota-sdk-ffi --lib --release
@@ -12,4 +12,14 @@ And finally, generate the Go bindings
 
 ```sh
 uniffi-bindgen-go --library target/release/libiota_sdk_ffi.dylib --out-dir ./bindings/go --no-format
+```
+
+To test it
+
+```sh
+BINARIES_DIR="../../target/release" \
+LD_LIBRARY_PATH="$BINARIES_DIR" \
+CGO_LDFLAGS="-liota_sdk_ffi -L$BINARIES_DIR" \
+CGO_ENABLED=1 \
+go run test.go
 ```
---
+++ bindings/go/test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	sdk "example.com/bindings/iota_sdk_ffi"
+)
+
+func main() {
+	fmt.Println("HELLO")
+	client := sdk.GraphQlClientNewDevnet()
+
+	chainID, err := client.ChainId()
+	// if err != nil {
+	// 	log.Fatalf("Failed to get chain ID: %#v (%T)", err, err)
+	// }
+	fmt.Println("Chain ID:", chainID)
+
+	myAddress, err := sdk.AddressFromHex("0xb14f13f5343641e5b52d144fd6f106a7058efe2f1ad44598df5cda73acf0101f")
+	if err != nil {
+		log.Fatalf("Failed to parse address: %v", err)
+	}
+
+	// cursor := ""
+	// limit := int32(0)
+	// pagination := sdk.PaginationFilter{
+	// 	Direction: sdk.DirectionForward,
+	// 	Cursor:    &cursor,
+	// 	Limit:     &limit,
+	// }
+
+	// coins, err := client.Coins(myAddress, pagination, nil)
+	// if err != nil {
+	// 	log.Fatalf("Failed to get coins: %v", err)
+	// }
+
+	// // for _, coin := range coins.Data {
+	// // 	fmt.Printf("ID = 0x%s Balance = %d\n", coin.ID.ToHex(), coin.Balance)
+	// // }
+
+	balance, err := client.Balance(myAddress, nil)
+	// if err != nil {
+	// 	log.Fatalf("Failed to get balance: %v", err)
+	// }
+	fmt.Printf("Total Balance = %d\n", balance)
+
+	// atCheckpoint := uint64(3)
+	// // TODO check error
+	// inputObject, _ := sdk.ObjectIdFromHex("0xb14f13f5343641e5b52d144fd6f106a7058efe2f1ad44598df5cda73acf0101f")
+	// txFilter := sdk.TransactionsFilter{
+	// 	AtCheckpoint: &atCheckpoint,
+	// 	InputObject: &inputObject,
+	// }
+
+	// eventFilter := sdk.EventFilter{
+	// 	Sender: &myAddress,
+	// }
+
+	// _ = txFilter
+	// _ = eventFilter
+}
