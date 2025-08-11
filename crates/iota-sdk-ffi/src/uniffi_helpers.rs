b017042be5bc4d1be32e913de06d4188a138b6fd
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2025-08-11 13:46:23 +0200

    feat(iota-sdk-ffi): DryRunResult, Event, TransactionEvent records (#71)
    * feat(iota-sdk-ffi): records for: DryRunResult, Event, TransactionEvent, ObjectRef, Validator, BigInt, DateTime, PageInfo, ValidatorSet,  ValidatorConnection, GQLAddress, ValidatorCredentials, MoveObject
    
    * clippy
    
    * address review comments
    
    * still need derive_more::From for some objects
    
    * revert to uniffi::remote
    
    * fmt
    
    * fix merge

--- crates/iota-sdk-ffi/src/uniffi_helpers.rs
+++ crates/iota-sdk-ffi/src/uniffi_helpers.rs
@@ -39,6 +39,7 @@ define_paged_record!(CheckpointSummaryPage, CheckpointSummary);
 define_paged_record!(SignedTransactionPage, SignedTransaction);
 define_paged_record!(TransactionDataEffectsPage, TransactionDataEffects);
 define_paged_record!(DynamicFieldOutputPage, DynamicFieldOutput);
+define_paged_record!(EventPage, Event);
 define_paged_record!(ValidatorPage, Validator);
 
 macro_rules! define_paged_object {
@@ -69,7 +70,6 @@ macro_rules! define_paged_object {
     };
 }
 
-define_paged_object!(EventPage, Event);
 define_paged_object!(CoinPage, Coin);
 define_paged_object!(ObjectPage, Object);
 define_paged_object!(TransactionEffectsPage, TransactionEffects);
