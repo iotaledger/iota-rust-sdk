bb3b3746de7e0915729e9a662cf968e62916157c
Thibault Martinez <thibault@iota.org>
2025-08-11 13:26:03 +0200

    feat(ffi): TypeTag and StructTag (#83)
    * feat(ffi): TypeTag and StructTag
    
    * remove TODOs
    
    * remove duplicate methods
    
    * remove into_
    
    * vector/struct constructors
    
    * remove unused import
    
    * Add StructTag::new
    
    * import StructTag
    
    * clippy

--- crates/iota-sdk-ffi/src/types/type_tag.rs
+++ crates/iota-sdk-ffi/src/types/type_tag.rs
@@ -1,5 +1,181 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use std::sync::Arc;
+
+use crate::types::struct_tag::StructTag;
+
+/// Type of a move value
+///
+/// # BCS
+///
+/// The BCS serialized form for this type is defined by the following ABNF:
+///
+/// ```text
+/// type-tag = type-tag-u8 \
+///            type-tag-u16 \
+///            type-tag-u32 \
+///            type-tag-u64 \
+///            type-tag-u128 \
+///            type-tag-u256 \
+///            type-tag-bool \
+///            type-tag-address \
+///            type-tag-signer \
+///            type-tag-vector \
+///            type-tag-struct
+///
+/// type-tag-u8 = %x01
+/// type-tag-u16 = %x08
+/// type-tag-u32 = %x09
+/// type-tag-u64 = %x02
+/// type-tag-u128 = %x03
+/// type-tag-u256 = %x0a
+/// type-tag-bool = %x00
+/// type-tag-address = %x04
+/// type-tag-signer = %x05
+/// type-tag-vector = %x06 type-tag
+/// type-tag-struct = %x07 struct-tag
+/// ```
 #[derive(Clone, Debug, derive_more::From, uniffi::Object)]
 pub struct TypeTag(pub iota_types::TypeTag);
+
+#[uniffi::export]
+impl TypeTag {
+    #[inline]
+    pub fn is_u8(&self) -> bool {
+        self.0.is_u8()
+    }
+
+    #[inline]
+    pub fn is_u16(&self) -> bool {
+        self.0.is_u16()
+    }
+
+    #[inline]
+    pub fn is_u32(&self) -> bool {
+        self.0.is_u32()
+    }
+
+    #[inline]
+    pub fn is_u64(&self) -> bool {
+        self.0.is_u64()
+    }
+
+    #[inline]
+    pub fn is_u128(&self) -> bool {
+        self.0.is_u128()
+    }
+
+    #[inline]
+    pub fn is_u256(&self) -> bool {
+        self.0.is_u256()
+    }
+
+    #[inline]
+    pub fn is_bool(&self) -> bool {
+        self.0.is_bool()
+    }
+
+    #[inline]
+    pub fn is_address(&self) -> bool {
+        self.0.is_address()
+    }
+
+    #[inline]
+    pub fn is_signer(&self) -> bool {
+        self.0.is_signer()
+    }
+
+    #[inline]
+    pub fn is_vector(&self) -> bool {
+        self.0.is_vector()
+    }
+
+    #[inline]
+    pub fn as_vector_type_tag_opt(&self) -> Option<Arc<TypeTag>> {
+        self.0
+            .as_vector_type_tag_opt()
+            .cloned()
+            .map(Into::into)
+            .map(Arc::new)
+    }
+
+    #[inline]
+    pub fn as_vector_type_tag(&self) -> TypeTag {
+        self.0.as_vector_type_tag().clone().into()
+    }
+
+    #[inline]
+    pub fn is_struct(&self) -> bool {
+        self.0.is_struct()
+    }
+
+    #[inline]
+    pub fn as_struct_tag_opt(&self) -> Option<Arc<StructTag>> {
+        self.0
+            .as_struct_tag_opt()
+            .cloned()
+            .map(Into::into)
+            .map(Arc::new)
+    }
+
+    #[inline]
+    pub fn as_struct_tag(&self) -> StructTag {
+        self.0.as_struct_tag().clone().into()
+    }
+
+    #[uniffi::constructor]
+    pub fn u8() -> Self {
+        Self(iota_types::TypeTag::U8)
+    }
+
+    #[uniffi::constructor]
+    pub fn u16() -> Self {
+        Self(iota_types::TypeTag::U16)
+    }
+
+    #[uniffi::constructor]
+    pub fn u32() -> Self {
+        Self(iota_types::TypeTag::U32)
+    }
+
+    #[uniffi::constructor]
+    pub fn u64() -> Self {
+        Self(iota_types::TypeTag::U64)
+    }
+
+    #[uniffi::constructor]
+    pub fn u128() -> Self {
+        Self(iota_types::TypeTag::U128)
+    }
+
+    #[uniffi::constructor]
+    pub fn u256() -> Self {
+        Self(iota_types::TypeTag::U256)
+    }
+
+    #[uniffi::constructor]
+    pub fn bool() -> Self {
+        Self(iota_types::TypeTag::Bool)
+    }
+
+    #[uniffi::constructor]
+    pub fn address() -> Self {
+        Self(iota_types::TypeTag::Address)
+    }
+
+    #[uniffi::constructor]
+    pub fn signer() -> Self {
+        Self(iota_types::TypeTag::Signer)
+    }
+
+    #[uniffi::constructor]
+    pub fn vector(type_tag: &TypeTag) -> Self {
+        Self(iota_types::TypeTag::Vector(Box::new(type_tag.0.clone())))
+    }
+
+    #[uniffi::constructor]
+    pub fn struct_tag(struct_tag: &StructTag) -> Self {
+        Self(iota_types::TypeTag::Struct(Box::new(struct_tag.0.clone())))
+    }
+}
