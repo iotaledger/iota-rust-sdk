03cdcedfa91fa0d81c5bc6168a5c292b06f0bd51
Thibault Martinez <thibault@iota.org>
2025-08-11 13:50:26 +0200

    correct way of checking errors

--- bindings/go/test.go
+++ bindings/go/test.go
@@ -7,13 +7,20 @@ import (
 	sdk "example.com/bindings/iota_sdk_ffi"
 )
 
+func isNilError(err error) bool {
+	if sdkErr, ok := err.(*sdk.SdkFfiError); ok {
+		return sdkErr == nil
+	}
+	return false
+}
+
 func main() {
 	client := sdk.GraphQlClientNewDevnet()
 
 	chainID, err := client.ChainId()
-	// if err != nil {
-	// 	log.Fatalf("Failed to get chain ID: %#v (%T)", err, err)
-	// }
+	if !isNilError(err) {
+		log.Fatalf("Failed to get chain ID: %v", err)
+	}
 	fmt.Println("Chain ID:", chainID)
 
 	address, err := sdk.AddressFromHex("0xb14f13f5343641e5b52d144fd6f106a7058efe2f1ad44598df5cda73acf0101f")
@@ -28,18 +35,18 @@ func main() {
 	}
 
 	coins, err := client.Coins(address, pagination, nil)
-	// if err != nil {
-	// 	log.Fatalf("Failed to get coins: %v", err)
-	// }
+	if !isNilError(err) {
+		log.Fatalf("Failed to get coins: %v", err)
+	}
 
 	for _, coin := range coins.Data {
 		fmt.Printf("ID = 0x%s Balance = %d\n", coin.Id().ToHex(), coin.Balance())
 	}
 
 	balance, err := client.Balance(address, nil)
-	// if err != nil {
-	// 	log.Fatalf("Failed to get balance: %v", err)
-	// }
+	if !isNilError(err) {
+		log.Fatalf("Failed to get balance: %v", err)
+	}
 	fmt.Printf("Total Balance = %d\n", balance)
 
 	atCheckpoint := uint64(3)
