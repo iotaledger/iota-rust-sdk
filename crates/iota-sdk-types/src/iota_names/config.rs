eb88b92f6ad397923fe29cf6e220c2fde84e6043
Chloe Martin <chloedaughterofmars@gmail.com>
2025-09-02 11:03:52 +0200

    update for testnet

--- crates/iota-sdk-types/src/iota_names/config.rs
+++ crates/iota-sdk-types/src/iota_names/config.rs
@@ -27,8 +27,7 @@ pub struct IotaNamesConfig {
 impl Default for IotaNamesConfig {
     fn default() -> Self {
         // TODO change to mainnet https://github.com/iotaledger/iota/issues/6532
-        // TODO change to testnet https://github.com/iotaledger/iota/issues/6531
-        Self::devnet()
+        Self::testnet()
     }
 }
 
@@ -61,8 +60,6 @@ impl IotaNamesConfig {
 
     // TODO add mainnet https://github.com/iotaledger/iota/issues/6532
 
-    // TODO add testnet https://github.com/iotaledger/iota/issues/6531
-
     // Create a config based on the package and object ids published on devnet.
     pub fn devnet() -> Self {
         const PACKAGE_ADDRESS: &str =
@@ -75,6 +72,34 @@ impl IotaNamesConfig {
             "0xe00b2f2400c33b4dbd3081c4dcf2e289d0544caba23a3d130b264bd756403c07";
         const REVERSE_REGISTRY_ID: &str =
             "0x1c1da17843cc453ad4079b05ce55e103b7a8cdd4db6ab42dc367b47ed6d8994d";
+
+        let package_address = Address::from_str(PACKAGE_ADDRESS).unwrap();
+        let object_id = ObjectId::from_str(OBJECT_ID).unwrap();
+        let payments_package_address = Address::from_str(PAYMENTS_PACKAGE_ADDRESS).unwrap();
+        let registry_id = ObjectId::from_str(REGISTRY_ID).unwrap();
+        let reverse_registry_id = ObjectId::from_str(REVERSE_REGISTRY_ID).unwrap();
+
+        Self::new(
+            package_address,
+            object_id,
+            payments_package_address,
+            registry_id,
+            reverse_registry_id,
+        )
+    }
+
+    // Create a config based on the package and object ids published on testnet.
+    pub fn testnet() -> Self {
+        const PACKAGE_ADDRESS: &str =
+            "0x7fff6e95f385349bec98d17121ab2bfa3e134f2f0b1ccefc270313415f7835ea";
+        const OBJECT_ID: &str =
+            "0x7cab491740d51e0d75b26bf9984e49ba2e32a2d0694cabcee605543ed13c7dec";
+        const PAYMENTS_PACKAGE_ADDRESS: &str =
+            "0x6b1b01f4c72786a893191d5c6e73d3012f7529f86fdee3bc8c163323cee08441";
+        const REGISTRY_ID: &str =
+            "0x2dfc6f6d46ba55217425643a59dc85fe4d8ed273a9f74077bd0ee280dbb4f590";
+        const REVERSE_REGISTRY_ID: &str =
+            "0x3550bcacb793ef8b776264665e7c99fa3d897695ed664656aac693cf9cf9b76b";
 
         let package_address = Address::from_str(PACKAGE_ADDRESS).unwrap();
         let object_id = ObjectId::from_str(OBJECT_ID).unwrap();
