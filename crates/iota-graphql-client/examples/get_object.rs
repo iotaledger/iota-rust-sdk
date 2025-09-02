fc0a63869b24bf77ee052c5c43787e44a5d8cb48
Chloe Martin <chloedaughterofmars@gmail.com>
2025-09-02 16:44:46 +0200

    sync example

--- crates/iota-graphql-client/examples/get_object.rs
+++ crates/iota-graphql-client/examples/get_object.rs
@@ -6,7 +6,6 @@ use std::str::FromStr;
 use anyhow::{Context, Result};
 use iota_graphql_client::Client;
 use iota_types::ObjectId;
-use serde::Deserialize;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -15,33 +14,12 @@ async fn main() -> Result<()> {
     let object_id =
         ObjectId::from_str("0x20c056090c3dd1604fcfd7ea759781de650aa45323738e799365d0c28bebeb1e")?;
 
-    let object = client
-        .object(object_id, None)
+    let object_json = client
+        .move_object_contents(object_id, None)
         .await?
         .context("missing object")?;
 
-    let bytes = &object.as_struct().contents;
-
-    println!("{}", object.as_struct().type_);
-
-    let registration = bcs::from_bytes::<IotaNamesRegistration>(bytes)?;
-
-    println!("registration name: {}", registration.name_str);
+    println!("Domain: {}", object_json["domain_name"]);
 
     Ok(())
-}
-
-#[derive(Deserialize)]
-#[expect(unused)]
-struct IotaNamesRegistration {
-    id: ObjectId,
-    name: Name,
-    name_str: String,
-    expiration_timestamp: u64,
-}
-
-#[derive(Deserialize)]
-#[expect(unused)]
-struct Name {
-    labels: Vec<String>,
 }
