670bb25073b211d97a482c71c5b85ce203b3b8ad
/alex/ <alexander.schmidt@iota.org>
2025-09-08 09:56:29 +0200

    clean up

--- crates/iota-graphql-client/examples/coin_balances.rs
+++ crates/iota-graphql-client/examples/coin_balances.rs
@@ -5,48 +5,30 @@ use iota_graphql_client::{
     Client,
     error::Result,
     pagination::{Direction, PaginationFilter},
-    query_types::PageInfo,
 };
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let client = Client::new_devnet();
     let address = "0xb14f13f5343641e5b52d144fd6f106a7058efe2f1ad44598df5cda73acf0101f".parse()?;
-
-    let mut cursor = None;
-    loop {
-        let (
-            PageInfo {
-                end_cursor,
-                has_next_page,
-                ..
+    let (_, coins) = client
+        .coins(
+            address,
+            None,
+            PaginationFilter {
+                direction: Direction::Forward,
+                ..Default::default()
             },
-            coins,
-        ) = client
-            .coins(
-                address,
-                None,
-                PaginationFilter {
-                    direction: Direction::Forward,
-                    cursor,
-                    limit: Some(3),
-                },
-            )
-            .await?
-            .into_parts();
+        )
+        .await?
+        .into_parts();
 
-        for coin in &coins {
-            println!("Coin = {}, Balance = {}", coin.id(), coin.balance());
-        }
-
-        if !has_next_page {
-            break;
-        }
-        cursor = end_cursor;
+    for coin in &coins {
+        println!("Coin = {}, Balance = {}", coin.id(), coin.balance());
     }
 
     let balance = client.balance(address, None).await?.unwrap_or_default();
-
     println!("Total balance = {balance}");
+
     Ok(())
 }
