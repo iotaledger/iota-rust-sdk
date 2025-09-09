67d173b98ed9c6ba6e8034f2f9837918d7baf894
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2025-09-09 15:44:41 +0200

    feat(examples): add transactions_with_function example (#195)
    * feat(examples): add transactions_with_function example
    
    * Use Result from error mod
    
    * update examples to latest changes

---
+++ bindings/go/examples/transactions_with_function/main.go
@@ -0,0 +1,27 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"fmt"
+	"log"
+
+	sdk "bindings/iota_sdk_ffi"
+)
+
+func main() {
+	client := sdk.GraphQlClientNewDevnet()
+
+	function := "0x3::iota_system::request_add_stake"
+	transactions, err := client.Transactions(&sdk.TransactionsFilter{
+		Function: &function,
+	}, nil)
+	if err.(*sdk.SdkFfiError) != nil {
+		log.Fatalf("Failed to get transactions: %v", err)
+	}
+
+	for _, transaction := range transactions.Data {
+		fmt.Println("Digest:", transaction.Transaction.Digest().ToBase58())
+	}
+}
--- crates/iota-graphql-client/examples/transactions_with_function.rs
+++ crates/iota-graphql-client/examples/transactions_with_function.rs
@@ -1,24 +1,21 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use std::str::FromStr;
-
-use iota_graphql_client::{Client, error::Result, query_types::TransactionsFilter};
-use iota_types::ObjectId;
+use iota_graphql_client::{
+    Client, error::Result, pagination::PaginationFilter, query_types::TransactionsFilter,
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let client = Client::new_devnet();
 
-    let shared_obj_id =
-        ObjectId::from_str("0x07c59b37bd7d036bf78fa30561a2ab9f7a970837487656ec29466e817f879342")?;
     let transactions = client
         .transactions(
             TransactionsFilter {
-                input_object: Some(shared_obj_id),
+                function: Some("0x3::iota_system::request_add_stake".to_string()),
                 ..Default::default()
             },
-            Default::default(),
+            PaginationFilter::default(),
         )
         .await?;
 
