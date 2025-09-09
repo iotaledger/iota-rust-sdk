fb0499132a8056c6d616a7e1b5790518aaeeac3e
/alex/ <alexander.schmidt@iota.org>
2025-09-09 14:53:02 +0200

    review

--- bindings/kotlin/examples/CoinBalances.kt
+++ bindings/kotlin/examples/CoinBalances.kt
@@ -6,21 +6,21 @@ import iota_sdk.GraphQlClient
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-        try {
-                val client = GraphQlClient.newDevnet()
-                val address =
-                        Address.fromHex(
-                                "0xb14f13f5343641e5b52d144fd6f106a7058efe2f1ad44598df5cda73acf0101f"
-                        )
+    try {
+        val client = GraphQlClient.newDevnet()
+        val address =
+                Address.fromHex(
+                        "0xb14f13f5343641e5b52d144fd6f106a7058efe2f1ad44598df5cda73acf0101f",
+                )
 
-                val coins = client.coins(address)
-                for (coin in coins.data) {
-                        println("Coin = 0x${coin.id().toHex()} Balance = ${coin.balance()}")
-                }
-
-                val balance = client.balance(address)
-                println("Total Balance = $balance")
-        } catch (e: Exception) {
-                e.printStackTrace()
+        val coins = client.coins(address)
+        for (coin in coins.data) {
+            println("Coin = 0x${coin.id().toHex()} Balance = ${coin.balance()}")
         }
+
+        val balance = client.balance(address)
+        println("Total Balance = $balance")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
--- crates/iota-graphql-client/examples/coin_balances.rs
+++ crates/iota-graphql-client/examples/coin_balances.rs
@@ -11,19 +11,12 @@ use iota_graphql_client::{
 async fn main() -> Result<()> {
     let client = Client::new_devnet();
     let address = "0xb14f13f5343641e5b52d144fd6f106a7058efe2f1ad44598df5cda73acf0101f".parse()?;
-    let (_, coins) = client
-        .coins(
-            address,
-            None,
-            PaginationFilter {
-                direction: Direction::Forward,
-                ..Default::default()
-            },
-        )
-        .await?
-        .into_parts();
 
-    for coin in &coins {
+    for coin in client
+        .coins(address, None, Default::default())
+        .await?
+        .data()
+    {
         println!("Coin = {}, Balance = {}", coin.id(), coin.balance());
     }
 
