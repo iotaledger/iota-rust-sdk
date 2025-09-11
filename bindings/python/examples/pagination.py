d4e39e6f23975fb79ee9c8ce123888c42879f60e
/alex/ <alexander.schmidt@iota.org>
2025-09-11 14:27:55 +0200

    add py example

--- bindings/python/examples/pagination.py
+++ bindings/python/examples/pagination.py
@@ -2,11 +2,20 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import asyncio
-from lib.iota_sdk_ffi import Address, Direction, GraphQlClient, ObjectFilter, PaginationFilter
+from lib.iota_sdk_ffi import (
+    Address,
+    Direction,
+    GraphQlClient,
+    ObjectFilter,
+    PaginationFilter,
+)
+
 
 async def main():
     client = GraphQlClient.new_devnet()
-    address = Address.from_hex("0x611830d3641a68f94a690dcc25d1f4b0dac948325ac18f6dd32564371735f32c")
+    address = Address.from_hex(
+        "0x611830d3641a68f94a690dcc25d1f4b0dac948325ac18f6dd32564371735f32c"
+    )
 
     all_objects = []
     next_cursor = None
@@ -15,7 +24,7 @@ async def main():
         page = await client.objects(
             ObjectFilter(owner=address),
             # Limit to 1 to demonstrate pagination
-            PaginationFilter(direction=Direction.FORWARD, cursor=next_cursor, limit=1)
+            PaginationFilter(direction=Direction.FORWARD, cursor=next_cursor, limit=1),
         )
         all_objects.extend(page.data)
         if page.page_info.has_next_page:
@@ -25,6 +34,7 @@ async def main():
     print(f"{len(all_objects)} objects fetched:")
     for obj_id in all_objects:
         print(obj_id.object_id().to_hex())
+
 
 if __name__ == "__main__":
     asyncio.run(main())
