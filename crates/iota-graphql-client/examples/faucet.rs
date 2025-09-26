9aef7c6859594b3c3161ead48eff18d52b59f5ac
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2025-09-26 21:20:48 +0200

    feat: add faucet example (#215)
    * feat: add faucet example
    
    * CI: start local network in bindings examples run

---
+++ crates/iota-graphql-client/examples/faucet.rs
@@ -0,0 +1,26 @@
+// Copyright (c) 2025 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::Result;
+use iota_graphql_client::faucet::FaucetClient;
+use iota_types::Address;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let address =
+        Address::from_hex("0x0000a4984bd495d4346fa208ddff4f5d5e5ad48c21dec631ddebc99809f16900")?;
+    let faucet_receipt = FaucetClient::local().request_and_wait(address).await?;
+    if let Some(receipt) = faucet_receipt {
+        println!("Faucet receipt:");
+        for coin in &receipt.sent {
+            println!(
+                "  Coin ID: {}, Amount: {}, Digest: {}",
+                coin.id, coin.amount, coin.transfer_tx_digest
+            );
+        }
+    } else {
+        println!("Faucet receipt: None");
+    }
+
+    Ok(())
+}
