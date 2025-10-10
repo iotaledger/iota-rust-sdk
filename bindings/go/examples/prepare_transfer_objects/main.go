ff52ecb47e1f409abef0d49949ef2035942ddfda
DaughterOfMars <chloedaughterofmars@gmail.com>
2025-10-10 09:54:54 +0200

    chore(examples): Add `transfer_objects` example (#265)
    * chore(examples): Add `transfer_objects` example
    
    * missed ids

--- bindings/go/examples/prepare_transfer_objects/main.go
+++ bindings/go/examples/prepare_transfer_objects/main.go
@@ -9,16 +9,29 @@ import (
 	sdk "bindings/iota_sdk_ffi"
 )
 
+func objIdFromHex(hex string) *sdk.ObjectId {
+	id, err := sdk.ObjectIdFromHex(hex)
+	if err != nil {
+		log.Fatalf("Failed to parse object ID: %v", err)
+	}
+	return id
+}
+
 func main() {
 	client := sdk.GraphQlClientNewDevnet()
 
 	fromAddress, _ := sdk.AddressFromHex("0x611830d3641a68f94a690dcc25d1f4b0dac948325ac18f6dd32564371735f32c")
 
 	toAddress, _ := sdk.AddressFromHex("0x0000a4984bd495d4346fa208ddff4f5d5e5ad48c21dec631ddebc99809f16900")
-	amount := uint64(5000000000)
+
+	objsToTransfer := []*sdk.PtbArgument{
+		sdk.PtbArgumentObjectId(objIdFromHex("0xd04077fe3b6fad13b3d4ed0d535b7ca92afcac8f0f2a0e0925fb9f4f0b30c699")),
+		sdk.PtbArgumentObjectId(objIdFromHex("0x0b0270ee9d27da0db09651e5f7338dfa32c7ee6441ccefa1f6e305735bcfc7ab")),
+		sdk.PtbArgumentObjectId(objIdFromHex("0x8ef4259fa2a3499826fa4b8aebeb1d8e478cf5397d05361c96438940b43d28c9")),
+	}
 
 	builder := sdk.TransactionBuilderInit(fromAddress, client)
-	builder.SendIota(toAddress, &amount)
+	builder.TransferObjects(toAddress, objsToTransfer)
 
 	txn, err := builder.Finish()
 	if err.(*sdk.SdkFfiError) != nil {
@@ -35,12 +48,12 @@ func main() {
 	skipChecks := bool(false)
 	res, err := client.DryRunTx(txn, &skipChecks)
 	if err.(*sdk.SdkFfiError) != nil {
-		log.Fatalf("Failed to send IOTA: %v", err)
+		log.Fatalf("Failed to transfer objects: %v", err)
 	}
 
 	if res.Error != nil {
-		log.Fatalf("Failed to send IOTA: %v", *res.Error)
+		log.Fatalf("Failed to transfer objects: %v", *res.Error)
 	}
 
-	log.Print("Send IOTA dry run was successful!")
+	log.Print("Transfer objects dry run was successful!")
 }
