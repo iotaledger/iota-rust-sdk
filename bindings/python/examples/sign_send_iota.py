dec94149782ec7e7dcfb09d97e3aad9aa5313bb0
/alex/ <alexander.schmidt@iota.org>
2025-10-13 23:46:02 +0200

    because python 'black' formatter says so

--- bindings/python/examples/sign_send_iota.py
+++ bindings/python/examples/sign_send_iota.py
@@ -14,7 +14,7 @@ async def main():
             "0x0000a4984bd495d4346fa208ddff4f5d5e5ad48c21dec631ddebc99809f16900"
         )
 
-        private_key = Ed25519PrivateKey(b'\x00' * 32)
+        private_key = Ed25519PrivateKey(b"\x00" * 32)
         public_key = private_key.public_key()
         sender_address = public_key.derive_address()
         print(f"Sender address: {sender_address.to_hex()}")
