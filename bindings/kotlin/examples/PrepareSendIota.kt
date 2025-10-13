576b13f22e4630af2524bbc2347de20240503e31
Thoralf Müller <thoralf.mue@gmail.com>
2025-10-13 18:55:48 +0200

    4 spaces

--- bindings/kotlin/examples/PrepareSendIota.kt
+++ bindings/kotlin/examples/PrepareSendIota.kt
@@ -5,39 +5,39 @@ import iota_sdk.*
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-        try {
-                val client = GraphQlClient.newDevnet()
+    try {
+        val client = GraphQlClient.newDevnet()
 
-                val fromAddress =
-                        Address.fromHex(
-                                "0x611830d3641a68f94a690dcc25d1f4b0dac948325ac18f6dd32564371735f32c"
-                        )
-
-                val toAddress =
-                        Address.fromHex(
-                                "0x0000a4984bd495d4346fa208ddff4f5d5e5ad48c21dec631ddebc99809f16900"
-                        )
-
-                val builder = TransactionBuilder.init(fromAddress, client)
-
-                builder.sendIota(
-                        toAddress,
-                        5000000000uL,
+        val fromAddress =
+                Address.fromHex(
+                        "0x611830d3641a68f94a690dcc25d1f4b0dac948325ac18f6dd32564371735f32c"
                 )
 
-                val txn = builder.finish()
+        val toAddress =
+                Address.fromHex(
+                        "0x0000a4984bd495d4346fa208ddff4f5d5e5ad48c21dec631ddebc99809f16900"
+                )
 
-                println("Signing Digest: ${hexEncode(txn.signingDigest())}")
-                println("Txn Bytes: ${base64Encode(txn.toBytes())}")
+        val builder = TransactionBuilder.init(fromAddress, client)
 
-                val res = builder.dryRun()
+        builder.sendIota(
+                toAddress,
+                5000000000uL,
+        )
 
-                if (res.error != null) {
-                        throw Exception("Failed to send IOTA: ${res.error}")
-                }
+        val txn = builder.finish()
 
-                println("Send IOTA dry run was successful!")
-        } catch (e: Exception) {
-                e.printStackTrace()
+        println("Signing Digest: ${hexEncode(txn.signingDigest())}")
+        println("Txn Bytes: ${base64Encode(txn.toBytes())}")
+
+        val res = builder.dryRun()
+
+        if (res.error != null) {
+            throw Exception("Failed to send IOTA: ${res.error}")
         }
+
+        println("Send IOTA dry run was successful!")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
