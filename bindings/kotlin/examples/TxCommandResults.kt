bcca522ac39ad71089ac3d508872b20dc6dbe1aa
Thoralf Müller <thoralf.mue@gmail.com>
2025-10-14 13:56:08 +0200

    fmt with 4 spaces

--- bindings/kotlin/examples/TxCommandResults.kt
+++ bindings/kotlin/examples/TxCommandResults.kt
@@ -5,64 +5,64 @@ import iota_sdk.*
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-        try {
-                val client = GraphQlClient.newDevnet()
+    try {
+        val client = GraphQlClient.newDevnet()
 
-                val sender =
-                        Address.fromHex(
-                                "0x611830d3641a68f94a690dcc25d1f4b0dac948325ac18f6dd32564371735f32c"
-                        )
-
-                val builder = TransactionBuilder.init(sender, client)
-
-                val packageAddr = Address.stdLib()
-                val moduleName = Identifier("u64")
-                val functionName = Identifier("max")
-
-                builder.moveCall(
-                        packageAddr,
-                        moduleName,
-                        functionName,
-                        listOf(PtbArgument.u64(0u), PtbArgument.u64(1000u)),
-                        // Assign a name to the result of this command
-                        names = listOf("res0"),
+        val sender =
+                Address.fromHex(
+                        "0x611830d3641a68f94a690dcc25d1f4b0dac948325ac18f6dd32564371735f32c"
                 )
 
-                builder.moveCall(
-                        packageAddr,
-                        moduleName,
-                        functionName,
-                        listOf(PtbArgument.u64(1000u), PtbArgument.u64(2000u)),
-                        // Assign a name to the result of this command
-                        names = listOf("res1"),
-                )
+        val builder = TransactionBuilder.init(sender, client)
 
-                builder.splitCoins(
-                        PtbArgument.gas(),
-                        // Use the named results of previous commands to use as arguments
-                        listOf(PtbArgument.res("res0"), PtbArgument.res("res1")),
-                        // For nested results, a tuple or vec can be used to name them
-                        listOf("coin0", "coin1"),
-                )
+        val packageAddr = Address.stdLib()
+        val moduleName = Identifier("u64")
+        val functionName = Identifier("max")
 
-                // Use named results as arguments
-                builder.transferObjects(
-                        sender,
-                        listOf(PtbArgument.res("coin0"), PtbArgument.res("coin1")),
-                )
+        builder.moveCall(
+                packageAddr,
+                moduleName,
+                functionName,
+                listOf(PtbArgument.u64(0u), PtbArgument.u64(1000u)),
+                // Assign a name to the result of this command
+                names = listOf("res0"),
+        )
 
-                val txn = builder.finish()
+        builder.moveCall(
+                packageAddr,
+                moduleName,
+                functionName,
+                listOf(PtbArgument.u64(1000u), PtbArgument.u64(2000u)),
+                // Assign a name to the result of this command
+                names = listOf("res1"),
+        )
 
-                println("Signing Digest: ${hexEncode(txn.signingDigest())}")
-                println("Txn Bytes: ${base64Encode(txn.bcsSerialize())}")
+        builder.splitCoins(
+                PtbArgument.gas(),
+                // Use the named results of previous commands to use as arguments
+                listOf(PtbArgument.res("res0"), PtbArgument.res("res1")),
+                // For nested results, a tuple or vec can be used to name them
+                listOf("coin0", "coin1"),
+        )
 
-                val res = client.dryRunTx(txn, false)
-                if (res.error != null) {
-                        throw Exception("Failed to send tx: ${res.error}")
-                }
+        // Use named results as arguments
+        builder.transferObjects(
+                sender,
+                listOf(PtbArgument.res("coin0"), PtbArgument.res("coin1")),
+        )
 
-                println("Tx dry run was successful!")
-        } catch (e: Exception) {
-                e.printStackTrace()
+        val txn = builder.finish()
+
+        println("Signing Digest: ${hexEncode(txn.signingDigest())}")
+        println("Txn Bytes: ${base64Encode(txn.bcsSerialize())}")
+
+        val res = client.dryRunTx(txn, false)
+        if (res.error != null) {
+            throw Exception("Failed to send tx: ${res.error}")
         }
+
+        println("Tx dry run was successful!")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
