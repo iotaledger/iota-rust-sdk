101dd5a1c188c2b5aed604eb08c17811fcda0195
Thibault Martinez <thibault@iota.org>
2025-10-17 13:55:39 +0200

    infallible base64

--- bindings/go/examples/tx_command_results/main.go
+++ bindings/go/examples/tx_command_results/main.go
@@ -55,12 +55,8 @@ func main() {
 		log.Fatalf("Failed to create transaction: %v", err)
 	}
 
-	txnBase64, err := txn.ToBase64()
-	if err != nil {
-		log.Fatalf("Failed to serialize transaction: %v", err)
-	}
 	log.Printf("Signing Digest: %v", txn.SigningDigestHex())
-	log.Printf("Txn Bytes: %v", txnBase64)
+	log.Printf("Txn Bytes: %v", txn.ToBase64())
 
 	res, err := client.DryRunTx(txn, false)
 	if err.(*sdk.SdkFfiError) != nil {
