b6f5320c2d1b04b1399b9af0aaa72c0dbe3f9996
/alex/ <alexander.schmidt@iota.org>
2025-10-28 15:09:17 +0100

    revert blank

--- bindings/go/examples/generic_move_function/main.go
+++ bindings/go/examples/generic_move_function/main.go
@@ -56,6 +56,7 @@ func main() {
 	if err.(*sdk.SdkFfiError) != nil {
 		log.Fatalf("Failed to call generic Move function: %v", err)
 	}
+
 	if res.Error != nil {
 		log.Fatalf("Failed to call generic Move function: %v", *res.Error)
 	}
