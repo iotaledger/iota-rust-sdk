b2e31b9e1efc55d4dc08cd18fda255db1c758214
/alex/ <alexander.schmidt@iota.org>
2025-11-03 12:14:46 +0100

    review

--- crates/iota-transaction-builder/src/builder/mod.rs
+++ crates/iota-transaction-builder/src/builder/mod.rs
@@ -445,34 +445,25 @@ impl<C, L> TransactionBuilder<C, L> {
     /// # Example
     ///
     /// ```rust
-    /// # use iota_graphql_client::Client;
-    /// # use iota_transaction_builder::TransactionBuilder;
-    /// # use iota_types::Address;
+    /// use iota_graphql_client::Client;
+    /// use iota_transaction_builder::TransactionBuilder;
+    /// use iota_types::{Address, ObjectId};
     ///
-    /// #[tokio::main(flavor = "current_thread")]
-    /// async fn main() -> eyre::Result<()> {
-    ///     let client = Client::new_devnet();
-    ///     let from_address = Address::from_hex(
-    ///         "0x611830d3641a68f94a690dcc25d1f4b0dac948325ac18f6dd32564371735f32c",
-    ///     )?;
-    ///     let to_address = Address::from_hex(
-    ///         "0x0000a4984bd495d4346fa208ddff4f5d5e5ad48c21dec631ddebc99809f16900",
-    ///     )?;
-    ///     let coin = ObjectId::from_str(
-    ///         "0x8ef4259fa2a3499826fa4b8aebeb1d8e478cf5397d05361c96438940b43d28c9",
-    ///     )?;
-    ///     let gas_coin = ObjectId::from_str(
-    ///         "0x0b0270ee9d27da0db09651e5f7338dfa32c7ee6441ccefa1f6e305735bcfc7ab",
-    ///     )?;
+    /// # #[tokio::main(flavor = "current_thread")]
+    /// # async fn main() -> eyre::Result<()> {
+    /// let client = Client::new_devnet();
+    /// let from_address =
+    ///     Address::from_hex("0x611830d3641a68f94a690dcc25d1f4b0dac948325ac18f6dd32564371735f32c")?;
+    /// let to_address =
+    ///     Address::from_hex("0x0000a4984bd495d4346fa208ddff4f5d5e5ad48c21dec631ddebc99809f16900")?;
+    /// let coin =
+    ///     ObjectId::from_hex("0x8ef4259fa2a3499826fa4b8aebeb1d8e478cf5397d05361c96438940b43d28c9")?;
     ///
-    ///     let mut builder = TransactionBuilder::new(from_address).with_client(client);
-    ///     builder
-    ///         .send_coins([coin], to_address, 50000000000u64)
-    ///         .gas(gas_coin)
-    ///         .gas_budget(1000000000);
-    ///     let txn = builder.finish().await?;
-    ///     Ok(())
-    /// }
+    /// let mut builder = TransactionBuilder::new(from_address).with_client(client);
+    /// builder.send_coins([coin], to_address, 50000000000u64);
+    /// let txn = builder.finish().await?;
+    /// #    Ok(())
+    /// # }
     /// ```
     pub fn send_coins<T: PTBArgumentList, U: PTBArgument>(
         &mut self,
