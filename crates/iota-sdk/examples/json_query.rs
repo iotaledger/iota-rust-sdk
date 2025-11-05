f6aaded5d42806c1df77981cd0cc539aff7dd6dd
Thoralf-M <thoralf.mue@gmail.com>
2025-11-05 13:09:39 +0200

    format query

--- crates/iota-sdk/examples/json_query.rs
+++ crates/iota-sdk/examples/json_query.rs
@@ -10,130 +10,130 @@ async fn main() -> Result<()> {
 
     let json = serde_json::json!({
         "query": r#"
-query getLatestIotaSystemState {
-  epoch {
-    epochId
-    startTimestamp
-    endTimestamp
-    referenceGasPrice
-    safeMode {
-      enabled
-      gasSummary {
-        computationCost
-        computationCostBurned
-        nonRefundableStorageFee
-        storageCost
-        storageRebate
-      }
-    }
-    storageFund {
-      nonRefundableBalance
-      totalObjectStorageRebates
-    }
-    systemStateVersion
-    iotaTotalSupply
-    iotaTreasuryCapId
-    systemParameters {
-      minValidatorCount
-      maxValidatorCount
-      minValidatorJoiningStake
-      durationMs
-      validatorLowStakeThreshold
-      validatorLowStakeGracePeriod
-      validatorVeryLowStakeThreshold
-    }
-    protocolConfigs {
-      protocolVersion
-    }
-    validatorSet {
-      activeValidators {
-        pageInfo {
-          hasNextPage
-          endCursor
-        }
-        nodes {
-          ...RPC_VALIDATOR_FIELDS
-        }
-      }
-      committeeMembers {
-        pageInfo {
-          hasNextPage
-          endCursor
-        }
-        nodes {
-          ...RPC_VALIDATOR_FIELDS
-        }
-      }
-      inactivePoolsSize
-      pendingActiveValidatorsSize
-      stakingPoolMappingsSize
-      validatorCandidatesSize
-      pendingRemovals
-      totalStake
-      stakingPoolMappingsId
-      pendingActiveValidatorsId
-      validatorCandidatesId
-      inactivePoolsId
-    }
-  }
-}
+          query getLatestIotaSystemState {
+            epoch {
+              epochId
+              startTimestamp
+              endTimestamp
+              referenceGasPrice
+              safeMode {
+                enabled
+                gasSummary {
+                  computationCost
+                  computationCostBurned
+                  nonRefundableStorageFee
+                  storageCost
+                  storageRebate
+                }
+              }
+              storageFund {
+                nonRefundableBalance
+                totalObjectStorageRebates
+              }
+              systemStateVersion
+              iotaTotalSupply
+              iotaTreasuryCapId
+              systemParameters {
+                minValidatorCount
+                maxValidatorCount
+                minValidatorJoiningStake
+                durationMs
+                validatorLowStakeThreshold
+                validatorLowStakeGracePeriod
+                validatorVeryLowStakeThreshold
+              }
+              protocolConfigs {
+                protocolVersion
+              }
+              validatorSet {
+                activeValidators {
+                  pageInfo {
+                    hasNextPage
+                    endCursor
+                  }
+                  nodes {
+                    ...RPC_VALIDATOR_FIELDS
+                  }
+                }
+                committeeMembers {
+                  pageInfo {
+                    hasNextPage
+                    endCursor
+                  }
+                  nodes {
+                    ...RPC_VALIDATOR_FIELDS
+                  }
+                }
+                inactivePoolsSize
+                pendingActiveValidatorsSize
+                stakingPoolMappingsSize
+                validatorCandidatesSize
+                pendingRemovals
+                totalStake
+                stakingPoolMappingsId
+                pendingActiveValidatorsId
+                validatorCandidatesId
+                inactivePoolsId
+              }
+            }
+          }
 
-fragment RPC_VALIDATOR_FIELDS on Validator {
-  address {
-    address
-  }
-  credentials {
-    authorityPubKey
-    networkPubKey
-    protocolPubKey
-    proofOfPossession
-    netAddress
-    p2PAddress
-    primaryAddress
-  }
-  nextEpochCredentials {
-    authorityPubKey
-    networkPubKey
-    protocolPubKey
-    proofOfPossession
-    netAddress
-    p2PAddress
-    primaryAddress
-  }
-  name
-  description
-  imageUrl
-  projectUrl
-  operationCap {
-    address
-  }
-  stakingPoolId
-  exchangeRatesTable {
-    address
-  }
-  exchangeRatesSize
-  stakingPoolActivationEpoch
-  stakingPoolIotaBalance
-  rewardsPool
-  poolTokenBalance
-  pendingStake
-  pendingTotalIotaWithdraw
-  pendingPoolTokenWithdraw
-  votingPower
-  gasPrice
-  commissionRate
-  nextEpochStake
-  nextEpochGasPrice
-  nextEpochCommissionRate
-  atRisk
-  reportRecords {
-    nodes {
-      address
-    }
-  }
-  apy
-}
-"#,
+          fragment RPC_VALIDATOR_FIELDS on Validator {
+            address {
+              address
+            }
+            credentials {
+              authorityPubKey
+              networkPubKey
+              protocolPubKey
+              proofOfPossession
+              netAddress
+              p2PAddress
+              primaryAddress
+            }
+            nextEpochCredentials {
+              authorityPubKey
+              networkPubKey
+              protocolPubKey
+              proofOfPossession
+              netAddress
+              p2PAddress
+              primaryAddress
+            }
+            name
+            description
+            imageUrl
+            projectUrl
+            operationCap {
+              address
+            }
+            stakingPoolId
+            exchangeRatesTable {
+              address
+            }
+            exchangeRatesSize
+            stakingPoolActivationEpoch
+            stakingPoolIotaBalance
+            rewardsPool
+            poolTokenBalance
+            pendingStake
+            pendingTotalIotaWithdraw
+            pendingPoolTokenWithdraw
+            votingPower
+            gasPrice
+            commissionRate
+            nextEpochStake
+            nextEpochGasPrice
+            nextEpochCommissionRate
+            atRisk
+            reportRecords {
+              nodes {
+                address
+              }
+            }
+            apy
+          }
+        "#,
         "variables": {}
     })
     .as_object()
