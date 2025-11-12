c42cdb82c2c40eb5ff00bf6f0fe6a983fa4257b7
Thibault Martinez <thibault@iota.org>
2025-11-12 18:14:39 +0100

    try github URL

--- bindings/go/examples/chain_id/main.go
+++ bindings/go/examples/chain_id/main.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 	"log"
 
-	sdk "bindings/iota_sdk_ffi"
+	sdk "github.com/iotaledger/iota-rust-sdk/bindings/go/iota_sdk_ffi"
 )
 
 func main() {
--- bindings/go/go.mod
+++ bindings/go/go.mod
@@ -1,3 +1,3 @@
-module bindings
+module github.com/iotaledger/iota-rust-sdk/bindings/go
 
 go 1.24.6
