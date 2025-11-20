da406b97e90cfebb08ca177e6e6a44bbbea36f77
Thoralf Müller <thoralf.mue@gmail.com>
2025-11-20 11:42:16 +0100

    filter by path

--- .github/gen-changelog.sh
+++ .github/gen-changelog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sed -i '1i\\' "$CRATE_ROOT/CHANGELOG.md"  # Prepend a newline for separation
-git-cliff --unreleased --tag "$NEW_VERSION" --prepend "$CRATE_ROOT/CHANGELOG.md"
+git-cliff --unreleased --tag "$NEW_VERSION" --include-path "$CRATE_ROOT" --prepend "$CRATE_ROOT/CHANGELOG.md"
 git add "*CHANGELOG.md"
