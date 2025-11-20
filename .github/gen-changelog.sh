fdaa586cd24ba5bec2c0997e94bfb36c3bc7abbf
Thoralf Müller <thoralf.mue@gmail.com>
2025-11-20 11:32:54 +0100

    fix gen-changelog.sh

--- .github/gen-changelog.sh
+++ .github/gen-changelog.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-git-cliff --unreleased --prepend -o "$CRATE_ROOT/CHANGELOG.md"
+sed -i '1i\\' "$CRATE_ROOT/CHANGELOG.md"  # Prepend a newline for separation
+git-cliff --unreleased --tag "$NEW_VERSION" --prepend "$CRATE_ROOT/CHANGELOG.md"
 git add "*CHANGELOG.md"
