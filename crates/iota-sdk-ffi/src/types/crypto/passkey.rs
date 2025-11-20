6a412ba585ad465a53ad7dbbdb02d7bf2811f938
/alex/ <alexander.schmidt@iota.org>
2025-11-20 12:13:19 +0100

    passkey

--- crates/iota-sdk-ffi/src/types/crypto/passkey.rs
+++ crates/iota-sdk-ffi/src/types/crypto/passkey.rs
@@ -37,7 +37,8 @@ use crate::{
 /// signature is ever embedded in another structure it generally is serialized
 /// as `bytes` meaning it has a length prefix that defines the length of
 /// the completely serialized signature.
-#[derive(derive_more::From, uniffi::Object)]
+#[derive(Debug, PartialEq, Eq, derive_more::From, uniffi::Object)]
+#[uniffi::export(Debug, Eq)]
 pub struct PasskeyAuthenticator(pub iota_sdk::types::PasskeyAuthenticator);
 
 #[uniffi::export]
@@ -88,7 +89,8 @@ impl PasskeyAuthenticator {
 /// ```text
 /// passkey-public-key = passkey-flag secp256r1-public-key
 /// ```
-#[derive(derive_more::From, uniffi::Object)]
+#[derive(Debug, PartialEq, Eq, derive_more::From, uniffi::Object)]
+#[uniffi::export(Debug, Eq)]
 pub struct PasskeyPublicKey(iota_sdk::types::PasskeyPublicKey);
 
 #[uniffi::export]
