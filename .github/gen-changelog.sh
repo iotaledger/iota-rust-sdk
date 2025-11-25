96861d7aa44415327cc7b3dfe7776d3eb53a028f
Thoralf Müller <thoralf.mue@gmail.com>
2025-11-25 07:27:07 +0100

    provide path and version as arguments instead of relying on env vars

--- .github/gen-changelog.sh
+++ .github/gen-changelog.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sed -i '1i\\' "$CRATE_ROOT/CHANGELOG.md"  # Prepend a newline for separation
-git-cliff --unreleased --tag "$NEW_VERSION" --include-path "${CRATE_ROOT}/**" --prepend "$CRATE_ROOT/CHANGELOG.md"
+PACKAGE_ROOT=$1
+NEW_VERSION=$2
+
+sed -i '1i\\' "$PACKAGE_ROOT/CHANGELOG.md"  # Prepend a newline for separation
+git-cliff --unreleased --tag "$NEW_VERSION" --include-path "${PACKAGE_ROOT}/**" --prepend "$PACKAGE_ROOT/CHANGELOG.md"
 git add "*CHANGELOG.md"
