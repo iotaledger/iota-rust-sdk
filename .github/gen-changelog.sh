67c495ab140fd6672e43412bd5252d85efa7419f
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2025-11-25 11:34:47 +0200

    feat(workflows): add Kotlin part (#426)
    * feat(workflows): add Kotlin part
    
    * revert testing change
    
    * create first, then export
    
    * Use env.NEW_VERSION so it can be used for Python and Kotlin
    
    * rename ids
    
    * provide path and version as arguments instead of relying on env vars
    
    * remove obsolete export
    
    * Use CRATE_ROOT env var

--- .github/gen-changelog.sh
+++ .github/gen-changelog.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-git-cliff -o "$CRATE_ROOT/CHANGELOG.md" --tag "$NEW_VERSION"
+PACKAGE_ROOT=$1
+NEW_VERSION=$2
+
+sed -i '1i\\' "$PACKAGE_ROOT/CHANGELOG.md"  # Prepend a newline for separation
+git-cliff --unreleased --tag "$NEW_VERSION" --include-path "${PACKAGE_ROOT}/**" --prepend "$PACKAGE_ROOT/CHANGELOG.md"
 git add "*CHANGELOG.md"
