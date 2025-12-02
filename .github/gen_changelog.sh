dfcc56bc3ce5d8be81310d62b38e04690e2b0220
Thibault Martinez <thibault@iota.org>
2025-12-02 18:01:01 +0100

    release.yml cleanup

--- .github/gen_changelog.sh
+++ .github/gen_changelog.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PKG=$1
-PKG_ROOT=$2
+PACKAGE=$1
+PACKAGE_ROOT=$2
 COMMIT_TO=$3
 VERSION=$4
 ADD=$5
 
 git fetch --tags
 
-LATEST_TAG=$(git tag -l "$PKG-v*" --sort=-v:refname | head -n 1)
+LATEST_TAG=$(git tag -l "$PACKAGE-v*" --sort=-v:refname | head -n 1)
 echo "Latest tag: $LATEST_TAG"
 
 COMMIT_FROM=$(git show-ref $LATEST_TAG | cut -f 1 -d' ')
@@ -16,10 +16,10 @@ echo "Commit from: $COMMIT_FROM"
 
 echo "Commit to: $COMMIT_TO"
 
-ENTRY=$(git-cliff $COMMIT_FROM..$COMMIT_TO --tag $VERSION --include-path $PKG_ROOT)
+ENTRY=$(git-cliff $COMMIT_FROM..$COMMIT_TO --tag $VERSION --include-path $PACKAGE_ROOT)
 echo "$ENTRY"
 
 if [ "$ADD" = "true" ]; then
-    echo -e "$ENTRY\n$(cat $PKG_ROOT/CHANGELOG.md)" > $PKG_ROOT/CHANGELOG.md
-    git add $PKG/CHANGELOG.md
+    echo -e "$ENTRY\n$(cat $PACKAGE_ROOT/CHANGELOG.md)" > $PACKAGE_ROOT/CHANGELOG.md
+    git add $PACKAGE/CHANGELOG.md
 fi
