e28de2f842bbc4f1bd5726b970eb8d5cd2b8bac0
Thibault Martinez <thibault@iota.org>
2025-12-03 14:12:34 +0100

    feat(ci): bindings release trigger (#413)
    * feat(ci): bindings release trigger
    
    * nit
    
    * some renaming
    
    * filter rust publish by tag
    
    * some TODOs
    
    * feat(github): add bump_version.sh
    
    * dprint
    
    * feat(ci): refactor go into pre_publish/release workflows (#419)
    
    * feat(ci): refactor go into pre_publish/release workflows
    
    * use bump script
    
    * dprint
    
    * yolo
    
    * uncomment checkout
    
    * typo
    
    * default level
    
    * trigger
    
    * whatever
    
    * checkout
    
    * checkout again
    
    * ok ???
    
    * tmp
    
    * another comment
    
    * remove notes file for now
    
    * test
    
    * quote?
    
    * take version from release tag
    
    * go_publish cleanup
    
    * fetch tags
    
    * print tags
    
    * fetch tags
    
    * remove quote?
    
    * release nits
    
    * version required
    
    * uncomment all this
    
    * re-comment some
    
    * cleanup
    
    * feat(workflows): add Kotlin part (#426)
    
    * feat(workflows): add Kotlin part
    
    * revert testing change
    
    * create first, then export
    
    * Use env.NEW_VERSION so it can be used for Python and Kotlin
    
    * rename ids
    
    * provide path and version as arguments instead of relying on env vars
    
    * remove obsolete export
    
    * Use CRATE_ROOT env var
    
    * feat(workflows): add Python part (#418)
    
    * first draft with commitizen
    
    * update action.yml
    
    * add temp pyproject.toml
    
    * level
    
    * Update .github/workflows/pre_publish.yml
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    
    * rm alpha
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    
    * clean up script
    
    * ids unnecessary now
    
    * no need
    
    ---------
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    
    * gen_changelog as snake_case
    
    * harmonise workflow names
    
    * harmonize `startsWith`
    
    * checkout release tag or dispatch ref
    
    * also do a PR for go
    
    * dprint
    
    * Set changelog directory
    
    * nits
    
    * generate changelog instead of relying on file
    
    * include path
    
    * properly update changelogs
    
    * prepend entry
    
    * dprint
    
    * --notes
    
    * conditionally add
    
    * COMMIT_TO as param
    
    * remove git add
    
    * release.yml cleanup
    
    * ADD default to false
    
    * proper arguments to gen_changelog
    
    * dprint fmt
    
    * remove patch
    
    ---------
    
    Co-authored-by: Thoralf Müller <thoralf.mue@gmail.com>
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    Co-authored-by: /alex/ <alexander.schmidt@iota.org>

---
+++ .github/gen_changelog.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PACKAGE=$1
+PACKAGE_ROOT=$2
+COMMIT_TO=$3
+VERSION=$4
+ADD=${5:-"false"}
+
+git fetch --tags
+
+echo "Generating changelog for package: $PACKAGE version $VERSION at $PACKAGE_ROOT"
+
+LATEST_TAG=$(git tag -l "$PACKAGE-v*" --sort=-v:refname | head -n 1)
+echo "Latest tag: $LATEST_TAG"
+
+COMMIT_FROM=$(git show-ref $LATEST_TAG | cut -f 1 -d' ')
+echo "Commit from: $COMMIT_FROM"
+
+echo "Commit to: $COMMIT_TO"
+
+ENTRY=$(git-cliff $COMMIT_FROM..$COMMIT_TO --tag $VERSION --include-path $PACKAGE_ROOT)
+echo "$ENTRY"
+
+if [ "$ADD" = "true" ]; then
+    echo "git add $PACKAGE_ROOT/CHANGELOG.md"
+    echo -e "$ENTRY\n$(cat $PACKAGE_ROOT/CHANGELOG.md)" > $PACKAGE_ROOT/CHANGELOG.md
+    git add $PACKAGE_ROOT/CHANGELOG.md
+fi
