281ceb716a8703528ba8da7ed3728b2d13fa9bd0
Andrew Cullen <45826600+cyberphysic4l@users.noreply.github.com>
2025-12-09 15:11:39 +0000

    feat(types): add ChangeEpochV4 (#474)
    * initial commit
    
    * fix merge with develop
    
    * add flag for adjusting rewards
    
    * fix merge errors
    
    * add FFIs and bindings
    
    ---------
    
    Co-authored-by: Olivia Saa <olivia.saa@iota.org>
    Co-authored-by: Thibault Martinez <thibault@iota.org>

--- crates/iota-sdk-types/src/transaction/serialization.rs
+++ crates/iota-sdk-types/src/transaction/serialization.rs
@@ -167,7 +167,7 @@ mod transaction_kind {
 mod end_of_epoch {
     use super::*;
     use crate::transaction::{
-        AuthenticatorStateExpire, ChangeEpoch, ChangeEpochV2, ChangeEpochV3,
+        AuthenticatorStateExpire, ChangeEpoch, ChangeEpochV2, ChangeEpochV3, ChangeEpochV4,
         EndOfEpochTransactionKind,
     };
 
@@ -177,6 +177,7 @@ mod end_of_epoch {
         ChangeEpoch(&'a ChangeEpoch),
         ChangeEpochV2(&'a ChangeEpochV2),
         ChangeEpochV3(&'a ChangeEpochV3),
+        ChangeEpochV4(&'a ChangeEpochV4),
         AuthenticatorStateCreate,
         AuthenticatorStateExpire(&'a AuthenticatorStateExpire),
     }
@@ -187,6 +188,7 @@ mod end_of_epoch {
         ChangeEpoch(ChangeEpoch),
         ChangeEpochV2(ChangeEpochV2),
         ChangeEpochV3(ChangeEpochV3),
+        ChangeEpochV4(ChangeEpochV4),
         AuthenticatorStateCreate,
         AuthenticatorStateExpire(AuthenticatorStateExpire),
     }
@@ -196,6 +198,7 @@ mod end_of_epoch {
         ChangeEpoch(&'a ChangeEpoch),
         ChangeEpochV2(&'a ChangeEpochV2),
         ChangeEpochV3(&'a ChangeEpochV3),
+        ChangeEpochV4(&'a ChangeEpochV4),
         AuthenticatorStateCreate,
         AuthenticatorStateExpire(&'a AuthenticatorStateExpire),
     }
@@ -205,6 +208,7 @@ mod end_of_epoch {
         ChangeEpoch(ChangeEpoch),
         ChangeEpochV2(ChangeEpochV2),
         ChangeEpochV3(ChangeEpochV3),
+        ChangeEpochV4(ChangeEpochV4),
         AuthenticatorStateCreate,
         AuthenticatorStateExpire(AuthenticatorStateExpire),
     }
@@ -223,6 +227,9 @@ mod end_of_epoch {
                     Self::ChangeEpochV3(k) => {
                         ReadableEndOfEpochTransactionKindRef::ChangeEpochV3(k)
                     }
+                    Self::ChangeEpochV4(k) => {
+                        ReadableEndOfEpochTransactionKindRef::ChangeEpochV4(k)
+                    }
                     Self::AuthenticatorStateCreate => {
                         ReadableEndOfEpochTransactionKindRef::AuthenticatorStateCreate
                     }
@@ -236,6 +243,7 @@ mod end_of_epoch {
                     Self::ChangeEpoch(k) => BinaryEndOfEpochTransactionKindRef::ChangeEpoch(k),
                     Self::ChangeEpochV2(k) => BinaryEndOfEpochTransactionKindRef::ChangeEpochV2(k),
                     Self::ChangeEpochV3(k) => BinaryEndOfEpochTransactionKindRef::ChangeEpochV3(k),
+                    Self::ChangeEpochV4(k) => BinaryEndOfEpochTransactionKindRef::ChangeEpochV4(k),
 
                     Self::AuthenticatorStateCreate => {
                         BinaryEndOfEpochTransactionKindRef::AuthenticatorStateCreate
@@ -264,6 +272,9 @@ mod end_of_epoch {
                         ReadableEndOfEpochTransactionKind::ChangeEpochV3(k) => {
                             Self::ChangeEpochV3(k)
                         }
+                        ReadableEndOfEpochTransactionKind::ChangeEpochV4(k) => {
+                            Self::ChangeEpochV4(k)
+                        }
                         ReadableEndOfEpochTransactionKind::AuthenticatorStateCreate => {
                             Self::AuthenticatorStateCreate
                         }
@@ -278,6 +289,7 @@ mod end_of_epoch {
                         BinaryEndOfEpochTransactionKind::ChangeEpoch(k) => Self::ChangeEpoch(k),
                         BinaryEndOfEpochTransactionKind::ChangeEpochV2(k) => Self::ChangeEpochV2(k),
                         BinaryEndOfEpochTransactionKind::ChangeEpochV3(k) => Self::ChangeEpochV3(k),
+                        BinaryEndOfEpochTransactionKind::ChangeEpochV4(k) => Self::ChangeEpochV4(k),
 
                         BinaryEndOfEpochTransactionKind::AuthenticatorStateCreate => {
                             Self::AuthenticatorStateCreate
