6f4e7f310c451bdc4ab91237783f1671b93dd7de
/alex/ <alexander.schmidt@iota.org>
2026-01-15 16:33:12 +0100

    review

--- crates/iota-sdk-ffi/src/types/crypto/intent.rs
+++ crates/iota-sdk-ffi/src/types/crypto/intent.rs
@@ -211,8 +211,7 @@ pub fn intent_prefix_length() -> u64 {
 
 /// A 1-byte domain separator for hashing Object ID in IOTA. It starts from
 /// 0xf0 to ensure no hashing collision for any ObjectID vs IotaAddress which is
-/// derived as the hash of `flag || pubkey`. See
-/// `iota_types::crypto::SignatureScheme::flag()`.
+/// derived as the hash of `flag || pubkey`.
 #[uniffi::remote(Enum)]
 #[uniffi::export(Eq, Debug, Hash)]
 #[non_exhaustive]
