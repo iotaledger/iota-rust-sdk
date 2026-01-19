94efc0fb39199e66dbb67ca326daa0a8461ac8c0
Thibault Martinez <thibault@iota.org>
2026-01-19 15:28:39 +0100

    add examples

---
+++ bindings/python/examples/json.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2026 IOTA Stiftung
+# SPDX-License-Identifier: Apache-2.0
+
+# This example demonstrates how to convert a Transaction to and from JSON.
+# A similar roundtrip can be done for other types as well.
+
+from lib.iota_sdk import *
+
+
+def main():
+    # A sample transaction in base64 format
+    tx_bytes_base64 = "AAACACAAAKSQS9SV1DRvogjd/09dXlrUjCHexjHd68mYCfFpAAAIAPIFKgEAAAACAgABAQEAAQECAAABAABhGDDTZBpo+UppDcwl0fSw2slIMlrBj23TJWQ3FzXzLAILAnDunSfaDbCWUeX3M436MsfuZEHM76H24wVzW8/Hq3M6MhwAAAAAIKlI7704HwxEKcAJUDavYxFuJgvpsFwQktqa3/trEI4n0EB3/jtvrROz1O0NU1t8qSr8rI8PKg4JJfufTwswxplyOjIcAAAAACBwo4RInFHkslDFUznEltYw/OPcH4EFo0/At7kMLZpocGEYMNNkGmj5SmkNzCXR9LDayUgyWsGPbdMlZDcXNfMs6AMAAAAAAACgLS0AAAAAAAA="
+
+    # Parse the transaction from base64
+    transaction = Transaction.from_base64(tx_bytes_base64)
+
+    # Convert the transaction to JSON
+    json = transaction_to_json(transaction)
+    print(f"Transaction as JSON:\n{json}")
+
+    # Convert the JSON back to a Transaction
+    parsed_transaction = transaction_from_json(json)
+    print(f"Parsed transaction back from JSON: {parsed_transaction}")
+
+
+if __name__ == "__main__":
+    main()
