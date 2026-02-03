9ab1ea035e6f2fe9e722d82d68d8a9732b68fa39
Chloe Martin <chloedaughterofmars@gmail.com>
2026-02-03 08:54:13 +0100

    typos and fmt

--- bindings/kotlin/examples/AbstractAccount.kt
+++ bindings/kotlin/examples/AbstractAccount.kt
@@ -49,7 +49,8 @@ suspend fun setupAccount(client: GraphQlClient): ObjectId {
 
     // Fund the sender address for gas payment
     val faucet = FaucetClient.newLocalnet()
-    faucet.requestAndWaitForFinalized(sender, client) ?: throw Exception("Failed to request coins from faucet")
+    faucet.requestAndWaitForFinalized(sender, client)
+        ?: throw Exception("Failed to request coins from faucet")
 
     // Build the `publish` PTB
     var builder = TransactionBuilder(sender).withClient(client)
