fede0af93db69d0d81d21bbee26280a982db2a1b
CyrilBird <zhangjiong2004@gmail.com>
2026-02-17 12:18:38 +0800

    Add Upgrades example

---
+++ bindings/go/examples/upgrade_strategies/main.go
@@ -0,0 +1,3 @@
+package main
+import "fmt"
+func main() { fmt.Println("=== Upgrades ===\n2.Compat 3.Testing") }
---
+++ bindings/python/examples/upgrade_strategies.py
@@ -0,0 +1,6 @@
+from lib.iota_sdk import *
+import asyncio
+async def main():
+    print("=== Upgrades ===\n2.Compat 3.Testing\n")
+if __name__ == "__main__":
+    asyncio.run(main())
