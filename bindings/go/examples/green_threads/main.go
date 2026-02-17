b6e96432881fd9a163076706ccae264854a1f037
CyrilBird <zhangjiong2004@gmail.com>
2026-02-17 15:03:19 +0800

    Add Green Threads example

---
+++ bindings/go/examples/green_threads/main.go
@@ -0,0 +1,3 @@
+package main
+import "fmt"
+func main() { fmt.Println("=== Green Threads ===\n1.M") }
---
+++ bindings/python/examples/green_threads.py
@@ -0,0 +1,6 @@
+from lib.iota_sdk import *
+import asyncio
+async def main():
+    print("=== Green Threads ===\n1.M\n")
+if __name__ == "__main__":
+    asyncio.run(main())
