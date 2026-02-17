308ce75477ad48c6655d2c30f460c9dff35ad545
CyrilBird <zhangjiong2004@gmail.com>
2026-02-17 12:47:12 +0800

    Add Metadata example

---
+++ bindings/go/examples/metadata_management/main.go
@@ -0,0 +1,3 @@
+package main
+import "fmt"
+func main() { fmt.Println("=== Metadata ===\n1.Schema 2.Lineage 3.Catalog") }
---
+++ bindings/kotlin/examples/UmetadataUmanagement.kt
@@ -0,0 +1,2 @@
+package iota.sdk.examples
+suspend fun main() { println("=== Metadata ===\n1.Schema 2.Lineage 3.Catalog") }
