196cbc7076893580b63f96b5fef33e80443eb8ba
CyrilBird <zhangjiong2004@gmail.com>
2026-02-17 13:30:47 +0800

    Add Runbook Automation example

---
+++ bindings/kotlin/examples/UrunbookUautomation.kt
@@ -0,0 +1,2 @@
+package iota.sdk.examples
+suspend fun main() { println("=== Runbook Automation ===\n1.Procedures 2.Execution 3.Validation") }
---
+++ bindings/python/examples/runbook_automation.py
@@ -0,0 +1,6 @@
+from lib.iota_sdk import *
+import asyncio
+async def main():
+    print("=== Runbook Automation ===\n1.Procedures 2.Execution 3.Validation\n")
+if __name__ == "__main__":
+    asyncio.run(main())
