4aebe0c0121195308ea190843ffbbd1d1ef98aeb
CyrilBird <zhangjiong2004@gmail.com>
2026-02-17 15:03:27 +0800

    Add STM example

---
+++ bindings/go/examples/software_transactional/main.go
@@ -0,0 +1,3 @@
+package main
+import "fmt"
+func main() { fmt.Println("=== STM ===\n1.Atoms 2.Transactions 3.Conflicts") }
---
+++ bindings/kotlin/examples/UsoftwareUtransactional.kt
@@ -0,0 +1,2 @@
+package iota.sdk.examples
+suspend fun main() { println("=== STM ===\n1.Atoms 2.Transactions 3.Conflicts") }
