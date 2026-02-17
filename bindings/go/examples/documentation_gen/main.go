c6c234496044497b083a3c777315d05334c7b0f0
CyrilBird <zhangjiong2004@gmail.com>
2026-02-17 13:31:06 +0800

    Add Documentation Gen example

---
+++ bindings/go/examples/documentation_gen/main.go
@@ -0,0 +1,3 @@
+package main
+import "fmt"
+func main() { fmt.Println("=== Documentation Gen ===\n1.API 2.Diagrams 3.Examples") }
---
+++ bindings/kotlin/examples/UdocumentationUgen.kt
@@ -0,0 +1,2 @@
+package iota.sdk.examples
+suspend fun main() { println("=== Documentation Gen ===\n1.API 2.Diagrams 3.Examples") }
