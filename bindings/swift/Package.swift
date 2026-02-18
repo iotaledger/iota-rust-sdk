ac148d2fb63fe9d8ba53de10a3f39a35cb3435e7
Cyber <shaibuafeez@gmail.com>
2026-02-18 10:10:20 +0100

    refactor: auto-discover Swift examples instead of manual list

--- bindings/swift/Package.swift
+++ bindings/swift/Package.swift
@@ -1,50 +1,21 @@
 // swift-tools-version: 5.9
 
 import PackageDescription
+import Foundation
 
-// All example files in the examples directory
-let exampleNames = [
-    "abstract_account",
-    "address_from_mnemonic",
-    "chain_id",
-    "coin_balances",
-    "custom_query",
-    "dev_inspect",
-    "dry_run_bytes",
-    "dynamic_fields",
-    "epoch",
-    "faucet",
-    "gas_sponsor",
-    "gas_station",
-    "generate_ed25519_address",
-    "generate_mnemonic",
-    "generic_move_function",
-    "get_object",
-    "get_transaction",
-    "json_query",
-    "json_roundtrip",
-    "move_functions",
-    "move_view_call",
-    "objects_by_type",
-    "owned_objects",
-    "package_events",
-    "pagination",
-    "prepare_merge_coins",
-    "prepare_send_coins",
-    "prepare_send_iota",
-    "prepare_send_iota_multi",
-    "prepare_split_coins",
-    "prepare_transfer_objects",
-    "prepare_transfer_objects_offline",
-    "publish_upgrade",
-    "sign_send_iota",
-    "stake",
-    "transaction_signer_callback",
-    "transactions_with_function",
-    "transactions_with_shared",
-    "tx_command_results",
-    "unstake",
-]
+// Auto-discover example files from the examples directory
+let exampleNames: [String] = {
+    let examplesDir = URL(fileURLWithPath: #filePath)
+        .deletingLastPathComponent()
+        .appendingPathComponent("examples")
+    guard let files = try? FileManager.default.contentsOfDirectory(atPath: examplesDir.path) else {
+        return []
+    }
+    return files
+        .filter { $0.hasSuffix(".swift") }
+        .map { String($0.dropLast(6)) }
+        .sorted()
+}()
 
 let package = Package(
     name: "IotaSDK",
