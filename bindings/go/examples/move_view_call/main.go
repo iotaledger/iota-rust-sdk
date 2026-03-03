3a44a966766f620514e2f5bfd9790bb8df6ee154
/alex/ <alexander.schmidt@iota.org>
2026-03-03 11:17:59 +0100

    fix Go

---
+++ bindings/go/examples/move_view_call/main.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2026 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Temporarily disabled until Move View calls are supported on testnet.")
+}
