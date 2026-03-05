e8a5fad6c87482f44685ddf44479568b9825150e
Thibault Martinez <thibault@iota.org>
2026-03-05 10:31:08 +0100

    feat: `GasPayment` improvements (#995)

--- crates/iota-sdk-types/src/transaction/mod.rs
+++ crates/iota-sdk-types/src/transaction/mod.rs
@@ -125,7 +125,7 @@ impl TransactionExpiration {
 ///               u64                 ; price
 ///               u64                 ; budget
 /// ```
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "schemars", derive(schemars::JsonSchema))]
 #[cfg_attr(feature = "proptest", derive(test_strategy::Arbitrary))]
