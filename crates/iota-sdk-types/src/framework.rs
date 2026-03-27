9dfa55e3a2166ba831376ebcfae01c8caacadde1
Thibault Martinez <thibault@iota.org>
2026-03-27 14:59:54 +0100

    feat: MoveStruct improvements (#1034)
    * feat: MoveStruct improvements
    
    * more fns
    
    * set_coin_value_unsafe
    
    * set_clock_timestamp_ms_unsafe
    
    * fixes
    
    * add type_tag method
    
    * unsafe -> unchecked
    
    * fix id_opt
    
    * address review comments
    
    * into_parts
    
    * nits
    
    * struct_tag
    
    * bring back object_type
    
    * clippy
    
    * remove is_ methods
    
    * doc comments
    
    * remove setters

--- crates/iota-sdk-types/src/framework.rs
+++ crates/iota-sdk-types/src/framework.rs
@@ -30,7 +30,7 @@ impl Coin {
         match &object.data {
             super::ObjectData::Struct(move_struct) => {
                 let coin_type = move_struct
-                    .type_
+                    .object_type
                     .coin_type_opt()
                     .ok_or(CoinFromObjectError::NotACoin)?;
 
