05e03d666623f992b4dca4ce5c69e69d9ddb1e66
Thibault Martinez <thibault@iota.org>
2026-03-30 16:18:11 +0200

    feat: `GenesisTransaction` improvements (#1048)

--- crates/iota-sdk-types/src/transaction/mod.rs
+++ crates/iota-sdk-types/src/transaction/mod.rs
@@ -709,7 +709,7 @@ pub struct SystemPackage {
 /// ```text
 /// genesis-transaction = (vector genesis-object)
 /// ```
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "proptest", derive(test_strategy::Arbitrary))]
 #[cfg_attr(feature = "bcs-schema", derive(iota_bcs_schema::BcsSchema))]
