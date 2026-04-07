3c469c998add55120f4cd0da5411eee749b77e6c
Thibault Martinez <thibault@iota.org>
2026-04-07 13:36:56 +0200

    UnchangedSharedKind fix

--- crates/iota-sdk-types/src/effects/v1.rs
+++ crates/iota-sdk-types/src/effects/v1.rs
@@ -594,7 +594,11 @@ mod serialization {
     }
 
     #[derive(serde::Serialize, serde::Deserialize)]
-    #[serde(tag = "kind", rename_all = "snake_case")]
+    #[serde(
+        rename = "UnchangedSharedKind",
+        tag = "kind",
+        rename_all = "snake_case"
+    )]
     enum ReadableUnchangedSharedKind {
         ReadOnlyRoot {
             #[serde(with = "crate::_serde::ReadableDisplay")]
@@ -617,6 +621,7 @@ mod serialization {
     }
 
     #[derive(serde::Serialize, serde::Deserialize)]
+    #[serde(rename = "UnchangedSharedKind")]
     enum BinaryUnchangedSharedKind {
         ReadOnlyRoot { version: Version, digest: Digest },
         MutateDeleted { version: Version },
