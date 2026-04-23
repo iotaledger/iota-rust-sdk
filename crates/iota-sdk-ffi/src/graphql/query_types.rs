567d33fa08d2de87ff96f1dd95d7c6e583c06acb
Chloe Martin <chloedaughterofmars@gmail.com>
2026-04-23 16:54:31 +0200

    clippy

--- crates/iota-sdk-ffi/src/graphql/query_types.rs
+++ crates/iota-sdk-ffi/src/graphql/query_types.rs
@@ -307,12 +307,10 @@ impl From<Epoch> for iota_sdk::graphql_client::query_types::Epoch {
             net_inflow: value.net_inflow.map(|v| v.into()),
             protocol_configs: value.protocol_configs,
             reference_gas_price: value.reference_gas_price.map(|v| v.into()),
-            start_timestamp: iota_sdk::graphql_client::query_types::DateTime(
-                value.start_timestamp.to_string(),
-            ),
+            start_timestamp: iota_sdk::graphql_client::query_types::DateTime(value.start_timestamp),
             end_timestamp: value
                 .end_timestamp
-                .map(|ts| iota_sdk::graphql_client::query_types::DateTime(ts.to_string())),
+                .map(|ts| iota_sdk::graphql_client::query_types::DateTime(ts)),
             system_state_version: value.system_state_version,
             total_checkpoints: value.total_checkpoints,
             total_gas_fees: value.total_gas_fees.map(|v| v.into()),
