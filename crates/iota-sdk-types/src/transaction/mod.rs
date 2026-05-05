3d64bb6ddcddebd7d6b667d2fb2d0c8a60a6d17a
/alex/ <alexander.schmidt@iota.org>
2026-05-05 15:09:04 +0200

    feat: `ConsensusCommitPrologueV1` improvements (#1114)
    * feat: `TransactionExpiration` improvements (#998)
    
    * Rename `BinaryTransactionExpiration` for serde
    
    * un-lowercase
    
    * revert option serialization
    
    * fix schemars impls
    
    remove regression file
    
    * feat: `GasPayment` improvements (#995)
    
    * feat: update `ConsensusCommitPrologueV1` (#983)
    
    * Derive Hash
    
    * add `VersionAssignment` ctor
    
    * serde rename
    
    * camelCase
    
    * fix rename
    
    * for god's sake
    
    * pascal case
    
    * custom json schema
    
    * custom serde
    
    * fix ci
    
    * rename
    
    * clean up
    
    * tuple variants
    
    * another breakage fix
    
    * and another breakage fix
    
    * ...
    
    * fix
    
    ---------
    
    Co-authored-by: DaughterOfMars <chloedaughterofmars@gmail.com>
    Co-authored-by: Thibault Martinez <thibault@iota.org>

--- crates/iota-sdk-types/src/transaction/mod.rs
+++ crates/iota-sdk-types/src/transaction/mod.rs
@@ -370,7 +370,7 @@ impl EndOfEpochTransactionKind {
     }
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "proptest", derive(test_strategy::Arbitrary))]
 #[cfg_attr(feature = "bcs-schema", derive(iota_bcs_schema::BcsSchema))]
 #[non_exhaustive]
@@ -402,8 +402,7 @@ impl ConsensusDeterminedVersionAssignments {
 /// ```text
 /// cancelled-transaction = digest (vector version-assignment)
 /// ```
-#[derive(Clone, Debug, PartialEq, Eq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "proptest", derive(test_strategy::Arbitrary))]
 #[cfg_attr(feature = "bcs-schema", derive(iota_bcs_schema::BcsSchema))]
 pub struct CancelledTransaction {
@@ -416,19 +415,25 @@ pub struct CancelledTransaction {
 ///
 /// # BCS
 ///
-/// The BCS serialized form for this type is defined by the following ABNF:
+/// The BCS serialized form for this type is defined by the
+/// following ABNF:
 ///
 /// ```text
 /// version-assignment = object-id u64
 /// ```
-#[derive(Clone, Debug, PartialEq, Eq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "proptest", derive(test_strategy::Arbitrary))]
 #[cfg_attr(feature = "bcs-schema", derive(iota_bcs_schema::BcsSchema))]
 pub struct VersionAssignment {
     pub object_id: ObjectId,
-    #[cfg_attr(feature = "serde", serde(with = "crate::_serde::ReadableDisplay"))]
     pub version: Version,
+}
+
+impl VersionAssignment {
+    /// Creates a [`VersionAssignment`].
+    pub fn new(object_id: ObjectId, version: Version) -> Self {
+        Self { object_id, version }
+    }
 }
 
 /// V1 of the consensus commit prologue system transaction
@@ -441,7 +446,7 @@ pub struct VersionAssignment {
 /// consensus-commit-prologue-v1 = u64 u64 (option u64) u64 digest
 ///                                consensus-determined-version-assignments
 /// ```
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "proptest", derive(test_strategy::Arbitrary))]
 #[cfg_attr(feature = "bcs-schema", derive(iota_bcs_schema::BcsSchema))]
