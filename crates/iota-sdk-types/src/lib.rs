28eb303712c3698bc88f60b1fd71adcbe4721a52
Thibault Martinez <thibault@iota.org>
2026-05-07 12:53:32 +0200

    move doc

--- crates/iota-sdk-types/src/lib.rs
+++ crates/iota-sdk-types/src/lib.rs
@@ -307,12 +307,12 @@ macro_rules! def_is_as_into_opt {
             self.[< as_ $rename _opt >]().expect(&format!("not a {}", stringify!($variant)))
         }
 
-        #[doc = "Converts this into a " $rename:snake " if it is a " $variant:snake " variant, or returns `None` otherwise."]
         #[inline]
         pub fn [< as_ $rename _mut >](&mut self) -> &mut $inner {
             self.[< as_ $rename _mut_opt >]().expect(&format!("not a {}", stringify!($variant)))
         }
 
+        #[doc = "Converts this into a " $rename:snake " if it is a " $variant:snake " variant, or returns `None` otherwise."]
         #[inline]
         pub fn [< as_ $rename _opt >](&self) -> Option<&$inner> {
             #[allow(irrefutable_let_patterns)]
