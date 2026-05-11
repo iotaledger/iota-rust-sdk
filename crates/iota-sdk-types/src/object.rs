9c11e89af6c9b46495b2742e7b43fef9a3a2c8d7
Thibault Martinez <thibault@iota.org>
2026-05-11 13:29:34 +0200

    drop as_

--- crates/iota-sdk-types/src/object.rs
+++ crates/iota-sdk-types/src/object.rs
@@ -173,14 +173,14 @@ pub enum ObjectData {
 impl ObjectData {
     crate::def_is_as_into_opt!(Struct(MoveStruct), Package(MovePackage));
 
-    pub fn as_opt_object_type(&self) -> Option<&MoveObjectType> {
+    pub fn opt_object_type(&self) -> Option<&MoveObjectType> {
         match self {
             Self::Struct(m) => Some(m.object_type()),
             Self::Package(_) => None,
         }
     }
 
-    pub fn as_opt_struct_tag(&self) -> Option<StructTag> {
+    pub fn opt_struct_tag(&self) -> Option<StructTag> {
         match self {
             Self::Struct(m) => Some(m.struct_tag().clone()),
             Self::Package(_) => None,
