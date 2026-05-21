487ad20cc7cf2bda0ff32a825b1aff799d676dbc
Thibault Martinez <thibault@iota.org>
2026-05-21 14:00:58 +0200

    chore: warn `clippy::unused_async` (#1164)
    * chore: warn `clippy::unused_async`
    
    * remove async in doc tests
    
    * one more
    
    * make test sync
    
    * remove async in AGENTS file

--- crates/iota-sdk-grpc-client/src/api/state/owned_objects.rs
+++ crates/iota-sdk-grpc-client/src/api/state/owned_objects.rs
@@ -59,7 +59,7 @@ impl Client {
     /// # use iota_sdk_grpc_client::Client;
     /// # use iota_types::Address;
     /// # async fn example() -> Result<(), Box<dyn std::error::Error>> {
-    /// let client = Client::new("http://localhost:9000").await?;
+    /// let client = Client::new("http://localhost:9000")?;
     /// let owner: Address = "0x1".parse()?;
     ///
     /// let page = client
@@ -83,7 +83,7 @@ impl Client {
     /// # use iota_sdk_grpc_client::Client;
     /// # use iota_types::Address;
     /// # async fn example() -> Result<(), Box<dyn std::error::Error>> {
-    /// let client = Client::new("http://localhost:9000").await?;
+    /// let client = Client::new("http://localhost:9000")?;
     /// let owner: Address = "0x1".parse()?;
     ///
     /// let all = client
