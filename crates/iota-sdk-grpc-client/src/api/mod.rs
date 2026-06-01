6b3744d4c8b5001425b0648ad5a171f5c4417f04
Chloe Martin <chloedaughterofmars@gmail.com>
2026-06-01 11:47:07 +0200

    fix checkpoint data doc

--- crates/iota-sdk-grpc-client/src/api/mod.rs
+++ crates/iota-sdk-grpc-client/src/api/mod.rs
@@ -214,10 +214,8 @@ impl CheckpointResponse {
     /// # async fn example() -> Result<(), Box<dyn std::error::Error>> {
     /// use iota_sdk_grpc_client::CHECKPOINT_RESPONSE_CHECKPOINT_DATA;
     ///
-    /// let client = Client::new("http://localhost:9000")?;
-    /// let cp = client
-    ///     .get_checkpoint_latest(Some(CHECKPOINT_RESPONSE_CHECKPOINT_DATA.into()), None, None)
-    ///     .await?;
+    /// let client = Client::new_localnet()?;
+    /// let cp = client.get_checkpoint_latest(None, None).await?;
     /// let data = cp.body().checkpoint_data()?;
     /// # Ok(())
     /// # }
