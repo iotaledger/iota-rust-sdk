ee7785862ca63e3e8233a4e6696b7e75ed3d9c19
Chloe Martin <chloedaughterofmars@gmail.com>
2026-09-15 14:50:59 +0200

    update another example

--- crates/iota-sdk-graphql-client/src/api/coins.rs
+++ crates/iota-sdk-graphql-client/src/api/coins.rs
@@ -153,7 +153,7 @@ mod tests {
             // address on localnet.
             _ => return,
         };
-        let key = Ed25519PublicKey::random_with(rand::rng());
+        let key = Ed25519PublicKey::random();
         let address = key.derive_address();
         faucet
             .request_and_wait_for_finalized(address, &client)
